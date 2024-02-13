59f7444101fb6e51b77dbd50e5efd7a5608516dd
Baptiste Rouzic <rouzicb@gmail.com>
2024-02-13 09:20:10 +0100

    ajout javascript thème

--- pages/script.js
+++ pages/script.js
@@ -6,3 +6,12 @@ function changeStyle() {
         stylesheet.setAttribute("href", "style/stylentree.css");
     }
 }
+
+function changeStylePlats() {
+    var stylesheet = document.getElementById("stylesheet");
+    if (stylesheet.getAttribute("href") === "style/stylentree.css") {
+        stylesheet.setAttribute("href", "style/stylentreelight.css");
+    } else {
+        stylesheet.setAttribute("href", "style/stylentree.css");
+    }
+}
