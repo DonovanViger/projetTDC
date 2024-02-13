96ca239a74c992a3b718a550dd085702e9a8de96
Tanguy Poulain <tanguy.poulain@univ-rouen.fr>
2024-02-13 09:41:53 +0100

    Fusion des styles stylentreelight.css et styleplatslight.css dans styleplatslight.css

--- pages/script.js
+++ pages/script.js
@@ -1,9 +1,9 @@
 function changeStyle() {
     var stylesheet = document.getElementById("stylesheet");
-    if (stylesheet.getAttribute("href") === "style/stylentree.css") {
-        stylesheet.setAttribute("href", "style/stylentreelight.css");
+    if (stylesheet.getAttribute("href") === "style/style3.css") {
+        stylesheet.setAttribute("href", "style/styleplatslight.css");
     } else {
-        stylesheet.setAttribute("href", "style/stylentree.css");
+        stylesheet.setAttribute("href", "style/style3.css");
     }
 }
 
