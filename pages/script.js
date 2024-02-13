b497f2c738b77be71079f7acd50659c69e0e0246
Baptiste Rouzic <rouzicb@gmail.com>
2024-02-13 09:21:15 +0100

    changement javascript

--- pages/script.js
+++ pages/script.js
@@ -9,9 +9,9 @@ function changeStyle() {
 
 function changeStylePlats() {
     var stylesheet = document.getElementById("stylesheet");
-    if (stylesheet.getAttribute("href") === "style/stylentree.css") {
-        stylesheet.setAttribute("href", "style/stylentreelight.css");
+    if (stylesheet.getAttribute("href") === "style/style3.css") {
+        stylesheet.setAttribute("href", "style/styleplatslight.css");
     } else {
-        stylesheet.setAttribute("href", "style/stylentree.css");
+        stylesheet.setAttribute("href", "style/style3.css");
     }
 }
