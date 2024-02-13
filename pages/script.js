e30a957764db78c693753766fbceca61138a441f
Tanguy Poulain <tanguy.poulain@univ-rouen.fr>
2024-02-13 09:46:50 +0100

    Modification des noms des styles

--- pages/script.js
+++ pages/script.js
@@ -1,17 +1,8 @@
 function changeStyle() {
     var stylesheet = document.getElementById("stylesheet");
-    if (stylesheet.getAttribute("href") === "style/style3.css") {
-        stylesheet.setAttribute("href", "style/styleplatslight.css");
+    if (stylesheet.getAttribute("href") === "style/style.css") {
+        stylesheet.setAttribute("href", "style/stylelight.css");
     } else {
-        stylesheet.setAttribute("href", "style/style3.css");
-    }
-}
-
-function changeStylePlats() {
-    var stylesheet = document.getElementById("stylesheet");
-    if (stylesheet.getAttribute("href") === "style/style3.css") {
-        stylesheet.setAttribute("href", "style/styleplatslight.css");
-    } else {
-        stylesheet.setAttribute("href", "style/style3.css");
+        stylesheet.setAttribute("href", "style/style.css");
     }
 }
